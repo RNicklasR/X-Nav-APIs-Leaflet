2b2f5af27593da63ef70948b4c5026575f75e74a
Richard Nicklas Rzepka <rnicklas@lambda17.aulas.gsyc.es>
2015-04-09 14:42:07 +0200

    r

--- geoloc.js
+++ geoloc.js
@@ -1,31 +1,17 @@
 $(document).ready(function() {
-	//$("#geo").click(showPosition);
-	
-//lambda18.aulas.gsyc.es:8000
-	//function getLocation(){
-		/*if(navigator.geolocation){
- 			navigator.geolocation.getCurrentPosition(showPosition);
-		}else{
- 			$("#info").text("Geolocation is not supported by this browser.");
-		}*/
-
-	//	function showPosition(position) {
-			//$("#info").text("Latitude: " + position.coords.latitude + " Longitude: " + position.coords.longitude);
 			
 
-			var map = L.map('map').setView([40.2838, -3.8215], 16);
-			L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { //para mapquest: http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png
-				attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-			}).addTo(map);
-				var marker = L.marker([40.2838,-3.8215]).addTo(map);
-			marker.bindPopup("<b>Estas aqui. </b>").openPopup();
-			map.on('click', function(e) {
-    				//alert(e.latlng);
-  				//marker.bindPopup(e.latlng.toString()).openPopup();
-				L.marker(e.latlng).addTo(map).bindPopup("coords: "+e.latlng).openpopup();
-				//L.circle(e.latlng, radius).addTo(map);
-			});
-		//}
-	//}
+	var map = L.map('map').setView([40.2838, -3.8215], 16);
+	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { //para mapquest: http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png
+		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+	}).addTo(map);
+		var marker = L.marker([40.2838,-3.8215]).addTo(map);
+	marker.bindPopup("<b>Estas aqui. </b>").openPopup();
+	map.on('click', function(e) {
+
+		L.marker(e.latlng).addTo(map).bindPopup("coords: "+e.latlng).openPopup();
+
+	});
+
 
 });
